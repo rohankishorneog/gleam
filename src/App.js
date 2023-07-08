ad0cfec62ccf583dedfe0ca09964efed74e93283
rohankishorneog <rkn2601@gmail.com>
2023-07-08 13:56:33 +0530

    created the sidelink page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { Routes, Route } from "react-router-dom";
 import RequiresAuth from "./components/RequiresAuth";
 
 
-import TestingKela from "./components/TestingKela";
+
 import LandingPage from "./pages/landingPage/LandingPage";
 import LoginPage from "./pages/loginpage/LoginPage";
 import SignupPage from "./pages/signupPage/SignupPage";
 import Homepage from './pages/homepage/Homepage';
+import ExplorePage from "./pages/explore/ExplorePage";
+import BookmarksPage from "./pages/bookmarks/BookmarksPage";
+import ProfilePage from "./pages/profilepage/ProfilePage";
+import CreatePost from "./components/createPost/CreatePost";
 
 
 
@@ -19,12 +23,18 @@ function App() {
     {/* <Mockman/> */}
     <Routes>
           <Route path="/" element={<LandingPage/>}/>
-          <Route path='/test' element={
-          <RequiresAuth><TestingKela/></RequiresAuth>}/>
           <Route path='/login' element={<LoginPage/>}/>
           <Route path='/signup' element={<SignupPage/>}/>
           <Route path='/home' element={
           <RequiresAuth><Homepage/></RequiresAuth>}/>
+          <Route path='/explore' element={
+          <RequiresAuth><ExplorePage/></RequiresAuth>}/>
+          <Route path='/bookmark' element={
+          <RequiresAuth><BookmarksPage/></RequiresAuth>}/>
+          <Route path='/profile' element={
+          <RequiresAuth><ProfilePage/></RequiresAuth>}/>
+          <Route path='/createPost' element={
+          <RequiresAuth><CreatePost/></RequiresAuth>}/>
     </Routes>
   </div>
   );
