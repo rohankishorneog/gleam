b0cf11e4c77b04738c637fdf46b08d7dc8e472c3
rohankishorneog <rkn2601@gmail.com>
2023-07-09 21:42:30 +0530

    created the bookmarksPage

--- src/pages/bookmarks/BookmarksPage.jsx
+++ src/pages/bookmarks/BookmarksPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
 
 const BookmarksPage = () => {
-  return (
-    <div>BookmarksPage</div>
-  )
-}
+  const { bookmarks , removeBookmark} = useContext(UserContext);
 
-export default BookmarksPage
+    const handleRemove=(id)=>{
+        removeBookmark(id)
+    }
+
+
+  return (
+    <div>
+        <h1>Bookmarks</h1>
+      {bookmarks.map((bookmark) => (
+       
+        <div key={bookmark._id}>
+            { console.log(bookmark._id)}
+          <h3>{bookmark.username}</h3>
+          <p>{bookmark.content}</p>
+          <p>{bookmark.likeCount}</p>
+          <button onClick={()=>handleRemove(bookmark._id)}>remove</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BookmarksPage;
