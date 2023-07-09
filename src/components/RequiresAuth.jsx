37a7875cfb4e0c2291b86a61be471d5d4965e534
rohankishorneog <rkn2601@gmail.com>
2023-07-09 21:38:11 +0530

    fix bug in lRequiresAuth

--- src/components/RequiresAuth.jsx
+++ src/components/RequiresAuth.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import LoginPage from '../pages/loginpage/LoginPage';
 
 
 const RequiresAuth = ({ children }) => {
   const { loggedInUser } = useContext(AuthContext);
 
-  return loggedInUser!==null?children: <span>auth required</span>
+
+  return loggedInUser!==null?children: <LoginPage/>
 };
 
 
