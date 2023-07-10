4e7a9f20d6ce833549a982937a1f9d7aca9fe085
rohankishorneog <rkn2601@gmail.com>
2023-07-10 17:09:31 +0530

    changed set of users

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -14,6 +14,9 @@ export const users = [
     password: "adarshBalika123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    bio: "Bio for Adarsh Balika",
+    avatarImg: "https://robohash.org/avatar1.png",
+    url: "https://example.com/profile/adarshbalika"
   },
   {
     _id: uuid(),
@@ -23,6 +26,9 @@ export const users = [
     password: "johnDoe123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    bio: "Bio for John Doe",
+    avatarImg: "https://robohash.org/avatar2.png",
+    url: "https://example.com/profile/johndoe"
   },
   {
     _id: uuid(),
@@ -32,6 +38,9 @@ export const users = [
     password: "janeSmith123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    bio: "Bio for Jane Smith",
+    avatarImg: "https://robohash.org/avatar3.png",
+    url: "https://example.com/profile/janesmith"
   },
   {
     _id: uuid(),
@@ -41,6 +50,9 @@ export const users = [
     password: "michaelJohnson123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    bio: "Bio for Michael Johnson",
+    avatarImg: "https://robohash.org/avatar4.png",
+    url: "https://example.com/profile/michaeljohnson"
   },
   {
     _id: uuid(),
@@ -50,6 +62,129 @@ export const users = [
     password: "emilyAnderson123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
-  }
-  
+    bio: "Bio for Emily Anderson",
+    avatarImg: "https://robohash.org/avatar5.png",
+    url: "https://example.com/profile/emilyanderson"
+  },
+  {
+    _id: uuid(),
+    firstName: "Sarah",
+    lastName: "Williams",
+    username: "sarahwilliams",
+    password: "sarahWilliams123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    bio: "Bio for Sarah Williams",
+    avatarImg: "https://robohash.org/avatar6.png",
+    url: "https://example.com/profile/sarahwilliams"
+  },
+  {
+    _id: uuid(),
+    firstName: "David",
+    lastName: "Thompson",
+    username: "davidthompson",
+    password: "davidThompson123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    bio: "Bio for David Thompson",
+    avatarImg: "https://robohash.org/avatar7.png",
+    url: "https://example.com/profile/davidthompson"
+  },
+  {
+    _id: uuid(),
+    firstName: "Olivia",
+    lastName: "Brown",
+    username: "oliviabrown",
+    password: "oliviaBrown123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    bio: "Bio for Olivia Brown",
+    avatarImg: "https://robohash.org/avatar1.png",
+    url: "https://example.com/profile/oliviabrown"
+  },
+  {
+    _id: uuid(),
+    firstName: "Daniel",
+    lastName: "Miller",
+    username: "danielmiller",
+    password: "danielMiller123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    bio: "Bio for Daniel Miller",
+    avatarImg: "https://robohash.org/avatar2.png",
+    url: "https://example.com/profile/danielmiller"
+  },
+  {
+    _id: uuid(),
+    firstName: "Sophia",
+    lastName: "Martinez",
+    username: "sophiamartinez",
+    password: "sophiaMartinez123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    bio: "Bio for Sophia Martinez",
+    avatarImg: "https://robohash.org/avatar3.png",
+    url: "https://example.com/profile/sophiamartinez"
+  },
+  {
+    _id: uuid(),
+    firstName: "Ethan",
+    lastName: "Garcia",
+    username: "ethangarcia",
+    password: "ethanGarcia123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    bio: "Bio for Ethan Garcia",
+    avatarImg: "https://robohash.org/avatar4.png",
+    url: "https://example.com/profile/ethangarcia"
+  },
+  {
+    _id: uuid(),
+    firstName: "Ava",
+    lastName: "Lopez",
+    username: "avalopez",
+    password: "avaLopez123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    bio: "Bio for Ava Lopez",
+    avatarImg: "https://robohash.org/avatar5.png",
+    url: "https://example.com/profile/avalopez"
+  },
+  {
+    _id: uuid(),
+    firstName: "Noah",
+    lastName: "Lee",
+    username: "noahlee",
+    password: "noahLee123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    bio: "Bio for Noah Lee",
+    avatarImg: "https://robohash.org/avatar6.png",
+    url: "https://example.com/profile/noahlee"
+  },
+  {
+    _id: uuid(),
+    firstName: "Isabella",
+    lastName: "Hall",
+    username: "isabellahall",
+    password: "isabellaHall123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    bio: "Bio for Isabella Hall",
+    avatarImg: "https://robohash.org/avatar7.png",
+    url: "https://example.com/profile/isabellahall"
+  },
+  {
+    _id: uuid(),
+    firstName: "James",
+    lastName: "Adams",
+    username: "jamesadams",
+    password: "jamesAdams123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    bio: "Bio for James Adams",
+    avatarImg: "https://robohash.org/avatar1.png",
+    url: "https://example.com/profile/jamesadams"
+  },
 ];
+
